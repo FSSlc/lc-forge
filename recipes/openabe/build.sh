bd3b26d3da6392241009e675d7ced65a73ec38c9
FSSlc <fsslc235@gmail.com>
2026-01-29 23:34:10 +0800

    fix openabe build

--- recipes/openabe/build.sh
+++ recipes/openabe/build.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib64/pkgconfig
-./configure --prefix=$PREFIX \
-    --with-gmp-include=$BUILD_PREFIX/include --with-gmp-lib=$BUILD_PREFIX/lib \
-    --with-pbc-include=$BUILD_PREFIX/include/pbc --with-pbc-lib=$BUILD_PREFIX
-make install -j
+export PERL=${BUILD_PREFIX}/bin/perl
+export CPATH=$CPATH:${BUILD_PREFIX}/include
+export LOCAL_LIB_ROOT=${BUILD_PREFIX}/lib
+export LOCAL_INCLUDE=${BUILD_PREFIX}/include
+export LOCAL_INSTALL_BIN=${BUILD_PREFIX}/bin
+export BISON=${BUILD_PREFIX}/bin/bison
+
+. ./env
+
+make deps
+make src
+make test
+
+mkdir -p ${PREFIX}/bin ${PREFIX}/lib/openabe ${PREFIX}/include/openabe
+
+cp -r ${BUILD_PREFIX}/include/gmp*.h ${PREFIX}/include/openabe
+cp ${BUILD_PREFIX}/lib/libgmp* ${PREFIX}/lib/openabe
+
+cp -r deps/root/include/* ${PREFIX}/include/openabe
+cp -r deps/root/lib/* ${PREFIX}/lib/openabe
+
+cp cli/oabe_setup ${PREFIX}/bin
+cp cli/oabe_keygen ${PREFIX}/bin
+cp cli/oabe_enc ${PREFIX}/bin
+cp cli/oabe_dec ${PREFIX}/bin
+
