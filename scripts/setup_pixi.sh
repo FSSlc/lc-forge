d990df67cdfd002cc4b3aa0e57e9ce192bae37c9
lczyxs <lczyxs@gmail.com>
2026-05-16 16:12:34 +0000

    remove  setup_pixi

--- scripts/setup_pixi.sh
+++ scripts/setup_pixi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "$REPO_ROOT" || exit 1
+
 curl -fsSL https://pixi.sh/install.sh | sh
+
+echo 'eval "$(pixi completion --shell bash)"' >> ~/.bashrc
+
 source ~/.bashrc
 
-pixi  global install -e tools conda-build rattler-build conda-recipe-manager
+$HOME/.pixi/bin/pixi global install -e tools \
+    rattler-build conda-recipe-manager rattler-index conda conda-build
