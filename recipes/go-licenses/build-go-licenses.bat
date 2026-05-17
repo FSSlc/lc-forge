c4788ebb0553ee91eb50a886fae5cc82227712c9
lczyxs <lczyxs@gmail.com>
2026-05-17 06:33:11 +0000

    fix go-licenses failed

--- recipes/go-licenses/build-go-licenses.bat
+++ recipes/go-licenses/build-go-licenses.bat
@@ -1,6 +1,6 @@
 @echo on
 
-go build -v -o "%PREFIX%\bin\go-licenses.exe" ^
+go build -buildvcs=false -v -o "%PREFIX%\bin\go-licenses.exe" ^
     || exit 2
 
 "%PREFIX%\bin\go-licenses.exe" save . --save_path=license-files ^
--- recipes/go-licenses/build-go-licenses.sh
+++ recipes/go-licenses/build-go-licenses.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
-go build -v -o "${PREFIX}/bin/go-licenses"
+go build -buildvcs=false -v -o "${PREFIX}/bin/go-licenses"
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   # Build for osx-64 as this can also be run in cross-compilation mode.
-  GOARCH=amd64 go build -v -o go-licenses-native
+  GOARCH=amd64 go build -buildvcs=false -v -o go-licenses-native
   ./go-licenses-native save . --save_path=./license-files
 else
   go-licenses save . --save_path=./license-files
