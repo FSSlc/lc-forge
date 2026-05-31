f1b5f558f82add3836abe5f01ce296c12af4630d
lczyxs <lczyxs@gmail.com>
2026-05-31 11:09:29 +0000

    mv sccache ccache xxhash to recipes; add c-agent cmake cmake-no-system

---
+++ todo/cmake/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -ex
+
+cmake -LAH -G Ninja ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
+    -DCMAKE_DOC_DIR=${PREFIX}/share/doc/cmake-${MAJOR_MINOR} \
+    -DCURSES_INCLUDE_PATH=${PREFIX}/include \
+    -DCMAKE_USE_SYSTEM_LIBRARIES=ON \
+    -DCMAKE_USE_SYSTEM_JSONCPP=OFF \
+    -DCMAKE_USE_SYSTEM_LIBARCHIVE=OFF \
+    -DCMAKE_USE_SYSTEM_CPPDAP=OFF \
+    -DCMAKE_USE_SYSTEM_LIBRARY_JSONCPP=OFF \
+    -DCMAKE_USE_SYSTEM_LIBRARY_LIBARCHIVE=OFF \
+    -DCMAKE_USE_SYSTEM_LIBRARY_CPPDAP=OFF \
+    -DBUILD_CursesDialog=ON \
+    -DBUILD_QtDialog=OFF \
+    . || (cat TryRunResults.cmake; false)
+
+cmake --build . --target install -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  ctest --output-on-failure -j${CPU_COUNT} -R "CTestTestParallel|DOWNLOAD"
+fi
