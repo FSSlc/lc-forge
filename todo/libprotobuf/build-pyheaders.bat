a60e09f21bc18037f5bb6338704a05e48b0a741f
FSSlc <fsslc235@gmail.com>
2026-06-03 05:15:52 +0000

    mv innoextract to recipes; add gdb

--- todo/libprotobuf/build-pyheaders.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-
-robocopy python/ %LIBRARY_PREFIX%\include *.h /E
-if %ERRORLEVEL% GEQ 8 exit 1
-
--- todo/libprotobuf/build-pyheaders.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -exuo pipefail
-
-rsync -a --prune-empty-dirs --include '*/' --include '*.h' --exclude '*' python/ $PREFIX/include/
