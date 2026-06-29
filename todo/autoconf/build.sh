4e1c5501cc501196170a6a036c4e1388549ec2ff
clsxyz <xxslc816@gmail.com>
2026-06-29 22:43:45 +0000

    upgrade libtool autoconf automake libarchive elfutils, add openblas

---
+++ todo/autoconf/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+pushd build-aux
+  cp ${BUILD_PREFIX}/share/gnuconfig/config.* .
+popd
+
+./configure --prefix=${PREFIX}        \
+            --libdir=${PREFIX}/lib    \
+            --build=${BUILD}          \
+            --host=${HOST}            \
+            PERL="${PREFIX}/bin/perl"
+
+make -j${CPU_COUNT}
+if [[ "$build_platform" == "$target_platform" ]]; then
+  make check || { cat tests/testsuite.log; exit 1; }
+fi
+make install
+
+sed -i.bak "s@${BUILD_PREFIX}/bin/m4@${PREFIX}/bin/m4@g" ${PREFIX}/bin/autom4te
+rm ${PREFIX}/bin/autom4te.bak
