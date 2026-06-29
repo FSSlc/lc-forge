c5d3a58a323a666d6475005cb6453a76a8de8ae8
clsxyz <xxslc816@gmail.com>
2026-06-29 22:59:07 +0000

    rebuild libarchive elfutils

--- todo/libarchive/test/test.c
+++
@@ -1,8 +0,0 @@
-#include <archive.h>
-
-/* Dummy program to test basic compilation and link. */
-int main(int argc, const char **argv)
-{
-    struct archive *a = archive_read_new();
-    return archive_read_free(a);
-}
