a2c656368461b689335016f7743029f7279102ae
lczyxs <lczyxs@gmail.com>
2026-07-22 04:36:44 +0000

    add sysroot 2.17

--- todo/linux-sysroot/build-sysroot.sh
+++ todo/linux-sysroot/build-sysroot.sh
@@ -2,19 +2,16 @@
 
 mkdir -p ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot
 pushd ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot > /dev/null 2>&1
-cp -Rf "${SRC_DIR}"/binary-glibc/* .
+cp -Rf "${SRC_DIR}"/binary/* .
 mkdir -p usr/include
 cp -Rf "${SRC_DIR}"/binary-glibc-headers/include/* usr/include/
 cp -Rf "${SRC_DIR}"/binary-glibc-devel/* usr/
 cp -Rf "${SRC_DIR}"/binary-glibc-static/* usr/
-cp -Rf "${SRC_DIR}"/binary-glibc-common/* usr/
-cp -Rf "${SRC_DIR}"/binary-glibc-gconv-extra/* usr/
-cp -Rf "${SRC_DIR}"/binary-glibc-all-langpacks/* usr/
+cp -Rf "${SRC_DIR}"/binary-glibc-common/* .
 
+mkdir -p usr/lib
 mkdir -p usr/lib64
-if [[ $(compgen -G 'usr/lib/*') != "" ]]; then
-    mv usr/lib/* usr/lib64/
-fi
+mv usr/lib/* usr/lib64/
 rm -rf usr/lib
 ln -s $PWD/usr/lib64 $PWD/usr/lib
 
@@ -22,20 +19,6 @@ if [ -d "lib" ]; then
     mv lib/* lib64/
     rm -rf lib
 fi
-ln -s $PWD/lib64 $PWD/lib
-
-## Linking or building against libsnsl produces binaries that don't run on recent Linux distributions.
-## Libraries and headers removed here to prevent this. See
-## https://github.com/conda-forge/rasterio-feedstock/issues/220
-rm -f lib64/libnsl*.so*
-rm -f usr/lib64/libnsl.{a,so}
-rm -f usr/include/rpcsvc/ypclnt.h
-rm -f usr/include/rpcsvc/yp.h
-rm -f usr/include/rpcsvc/yppasswd.h
-rm -f usr/include/rpcsvc/yppasswd.x
-rm -f usr/include/rpcsvc/yp_prot.h
-rm -f usr/include/rpcsvc/ypupd.h
-rm -f usr/include/rpcsvc/yp.x
 
 if [[ "$target_machine" == "s390x" ]]; then
    rm -rf $PWD/lib64/ld64.so.1
@@ -45,10 +28,31 @@ fi
 mkdir -p usr/share
 ln -sf ${PREFIX}/share/zoneinfo usr/share/zoneinfo
 
-# we don't need these
-rm -rf usr/share/man
-rm -rf usr/lib/systemd
-rm -rf usr/share/doc
+## Linking or building against libsnsl produces binaries that don't run on recent Linux distributions.
+## Libraries and headers removed here to prevent this. See
+## https://github.com/conda-forge/rasterio-feedstock/issues/220
+rm lib64/libnsl*.so*
+rm usr/lib64/libnsl.{a,so}
+rm usr/include/rpcsvc/ypclnt.h
+rm usr/include/rpcsvc/yp.h
+rm usr/include/rpcsvc/yppasswd.h
+rm usr/include/rpcsvc/yppasswd.x
+rm usr/include/rpcsvc/yp_prot.h
+rm usr/include/rpcsvc/ypupd.h
+rm usr/include/rpcsvc/yp.x
+
+if [[ "$with_crypt" == "false" ]]; then
+    # we supply libcrypt/libxcrypt as a separate package
+    rm usr/include/crypt.h
+    ## In the (near) future we may want to remove all libcrypt*.so* files;
+    ## for now, just remove .a/.so so we stop linking new packages to libcrypt.so.1.
+    # rm lib64/libcrypt*.so*
+    rm usr/lib64/libcrypt.{a,so}
+fi
+
+ln -s $PWD/lib64 $PWD/lib
+
+cp "${SRC_DIR}"/binary-freebl/usr/lib64/libfreebl3.so ${PWD}/usr/lib64/.
 
 popd
 
