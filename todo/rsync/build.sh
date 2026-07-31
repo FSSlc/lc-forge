878f5196982b46b21ccb36a15d8f3680197fb93d
lczyxs <lczyxs@gmail.com>
2026-07-31 15:14:37 +0000

    add rsync

---
+++ todo/rsync/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e -o pipefail
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    EXTRA_CONFIGURE_ARGS="--disable-simd"
+fi
+./configure --prefix=$PREFIX --without-included-zlib --without-included-popt ${EXTRA_CONFIGURE_ARGS:-}
+make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    make check
+fi
+make install
