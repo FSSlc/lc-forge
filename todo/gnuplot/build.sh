de0c2ab211aba7b71ec4131102ee34a3ce2d821a
FSSlc <fsslc235@gmail.com>
2026-09-20 18:28:55 +0000

    add gnuplot

---
+++ todo/gnuplot/build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  # some tools are built and later executed in the build process
+  # this requires a native build first
+  (
+    mkdir -p build-native
+    pushd build-native
+    LDFLAGS_FOR_BUILD=$(echo $LDFLAGS | sed "s?$PREFIX?$BUILD_PREFIX?g")
+    LDFLAGS_LD_FOR_BUILD=$(echo $LDFLAGS_LD | sed "s?$PREFIX?$BUILD_PREFIX?g")
+    # CFLAGS -mcpu=power8 -mtune=power8 break the ppc64le build
+    CFLAGS_FOR_BUILD=$(echo $CFLAGS | sed -E 's?-mcpu=[^ ]+ ??; s?-mtune=[^ ]+ ??')
+    ../configure --prefix="${BUILD_PREFIX}" \
+     	--without-lua \
+      --without-latex \
+      --without-libcerf \
+      --with-qt=no \
+      --without-readline \
+      --without-cairo \
+      --disable-dependency-tracking \
+      CC=$CC_FOR_BUILD \
+      CXX=$CXX_FOR_BUILD \
+      AR=${build_alias}-ar \
+      LD=${build_alias}-ld \
+      RANLIB=${build_alias}-ranlib \
+      LDFLAGS="$LDFLAGS_FOR_BUILD" \
+      LDFLAGS_LD="$LDFLAGS_LD_FOR_BUILD" \
+      CFLAGS="$CFLAGS_FOR_BUILD"
+   
+    # Fix iconv linkage
+    sed -ie 's/\(^LIBS.*\)/\1 -liconv/g' ./src/Makefile
+    make -j${CPU_COUNT} PREFIX=$BUILD_PREFIX
+    make install PREFIX=$BUILD_PREFIX
+    popd
+  )
+fi
+
+if [[ $target_platform == "linux-ppc64le" ]]; then
+  BUILD_WITH_QTVER=no
+else
+  BUILD_WITH_QTVER=qt5
+fi
+
+./configure \
+	--prefix=$PREFIX \
+	--without-lua \
+	--without-latex \
+	--without-libcerf \
+	--with-qt=$BUILD_WITH_QTVER \
+	--with-readline=$PREFIX \
+	--disable-dependency-tracking
+
+export GNUTERM=dumb
+# Fix iconv linkage
+sed -ie 's/\(^LIBS.*\)/\1 -liconv/g' src/Makefile
+
+make -j${CPU_COUNT} PREFIX=$PREFIX
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  make check PREFIX=$PREFIX
+fi
+make install PREFIX=$PREFIX
